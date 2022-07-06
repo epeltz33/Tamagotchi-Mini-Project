7754182c0232c6d5ca1d124c3c8c47d03e9c745a
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-06 18:54:58 -0400

    started over

--- js/app.js
+++ js/app.js
@@ -1,30 +1,18 @@
-// Tamagotchi Mini-Project
+// Tamagotchi
 
-// Create a class for the Tamagotchi
+// Create an Object or Class (look at your notes on JS Classes if you forgot) for your Tamagotchi
+
 class Tamagotchi {
-  // Display the following properties on the Tamagotchi class:
-   constructor(name, age, hunger, sleepiness, boredom, image) {
-     this.name = name;
-     this.age = age;
-     this.hunger = hunger;
-     this.sleepiness = sleepiness;
-     this.boredom = boredom;
-      this.image = image;
-   }
+ constructor( name, age, hunger, sleepiness, boredom ) {
+   this.name = "";
+   this.hunger = 0;
+   this.sleep = 0;
+   this.boredom = 0;
+   this.age = 0;
+   this.alive = true;
  }
- // Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
- Tamagotchi.prototype.addPetButton = function() { 
-  const btn = document.getElementById('pet');
-    btn.addEventListener('click', function() {
-      this.hunger -= 10;
-      this.sleepiness -= 10;
-      this.boredom -= 10;
-      this.age += 1;
-      this.displayPet();
-    }
-    );
-  }
  
-
-
+   
+  
+}
 
