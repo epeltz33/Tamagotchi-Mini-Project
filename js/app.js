cb668314cdc53e2c268f5a96529060face388ff8
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-09 16:24:25 -0400

    html overhaul

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@
 
 const Tamagotchi = {
     initialize: function(name) {
-        
+     this.name = name; // name of the tamagotchi
+        this.hungerLevel = 10;
+        thi // hunger level of the tamagotchi (10 is full, 0 is dead)   
     }
 }
 
