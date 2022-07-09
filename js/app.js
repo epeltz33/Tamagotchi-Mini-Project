ef6a9168f39790ae60cfc2625beb333b87014f6f
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-09 16:35:34 -0400

    created class and methods

--- js/app.js
+++ js/app.js
@@ -2,12 +2,33 @@
 
 // Create an Object or Class (look at your notes on JS Classes if you forgot) for your Tamagotchi
 
-const Tamagotchi = {
-    initialize: function(name) {
-     this.name = name; // name of the tamagotchi
-        this.hungerLevel = 10;
-        thi // hunger level of the tamagotchi (10 is full, 0 is dead)   
+// Create a constructor function for your Tamagotchi
+
+class pet {
+    constructor(name, age, hunger, sleepiness, boredom) {
+        this.name = name;
+        this.age = age;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
+    }
+    // methods for your Tamagotchi that will update its stats
+    feed() {
+        this.hunger -= 10;
+    }
+    sleep() {
+        this.sleepiness -= 10;
+    }
+    play() {
+        this.boredom -= 10;
+    }
+    update() {
+        this.age += 1;
+        this.hunger += 10;
+        this.sleepiness += 10;
+        this.boredom += 10;
     }
 }
+
 
 
