c9f463734f6aa54e3aed812f81d5a21b8db369e5
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-10 12:44:04 -0400

    turn lights

--- js/app.js
+++ js/app.js
@@ -16,3 +16,47 @@ const Pet = {
 
 
 }
+// functions to  increase the stats of the pet
+const increaseHunger = () => {
+    Pet.hunger++;
+    updateHunger(); // update the hunger stat
+    console.log`${Pet.name} is hungry`;
+}
+const increaseSleep = () => {
+    Pet.sleep++;
+    updateSleep(); // update the sleep stat
+    console.log`${Pet.name} is sleepy`;
+}
+const increaseBoredom = () => {
+    Pet.boredom++;
+    updateBoredom(); // update the boredom stat
+    console.log`${Pet.name} is bored`;
+}
+const increaseAge = () => {
+    Pet.age++;
+    updateAge(); // update the age stat
+    console.log`${Pet.name} is old`;
+}
+// functions to  decrease the stats of the pet
+const decreaseHunger = () => {
+    Pet.hunger--;
+    updateHunger(); // update the hunger stat
+    console.log("That tasted good");
+}
+toggleLights = () => {
+    if (Pet.lightsOn) {
+        Pet.lightsOn = false;
+        background.style.backgroundColor = "rgba(255,255,255,0.5)"; // background color is turns on 
+        console.log("The lights on");
+    } else {
+        Pet.lightsOn = true;
+        background.style.backgroundColor = "rgba(0,0,0,0.5)"; 
+        Pet.sleep = 2;
+        updateSleep();
+        console.log("The lights off");
+        }
+        
+       
+}
+ 
+
