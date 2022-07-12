1ae101f09c022a8d7491ed554ff54661bc6e7889
Eric-Peltzman33 <erpeltz@gmail.com>
2022-07-12 17:18:24 -0400

    main

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 // create  a function that will display the name of the Tamagotchi'by creating a new div with the name of the Tamagotchi
 const retrieveTamagotchi = () => {
-  let tamagotchiName = document.getElementById('tamName').value;
-  let h3 = document.createElement('h3');
+  let tamagotchiName = document.getElementById("tamName").value;
+  let h3 = document.createElement("h3");
   h3.innerText = tamagotchiName;
-  h3.setAttribute('id', 'tamagotchiName');
+  h3.setAttribute("id", "tamagotchiName");
   document.body.appendChild(h3);
-
 };
-document.getElementById('submit').addEventListener('click', retrieveTamagotchi);
+document.getElementById("submit").addEventListener("click", retrieveTamagotchi);
 
 // create a class that will create the Tamagotchi with a constructor
 
@@ -24,7 +23,7 @@ class Tamagotchi {
   // making a function that will increase hunger by a set interval 2 seconds
   hungerIncrease() {
     this.hungerid = setInterval(() => {
-      const hungerFactor = document.getElementById('hunger');
+      const hungerFactor = document.getElementById("hunger");
       hungerFactor.innerText = `Hunger: ${this.hunger}`;
       this.hunger++;
 
@@ -38,7 +37,7 @@ class Tamagotchi {
   // making a function that will increase sleep by a set interval 1.5 seconds
   sleepIncrease() {
     this.sleepid = setInterval(() => {
-      const sleepFactor = document.getElementById('tired');
+      const sleepFactor = document.getElementById("tired");
       sleepFactor.innerText = `Sleep: ${this.sleep}`;
       this.sleep++;
 
@@ -52,141 +51,97 @@ class Tamagotchi {
 
   ageIncrease() {
     this.ageid = setInterval(() => {
-    
-      const ageFactor = document.getElementById('age');
-      
+      const ageFactor = document.getElementById("age");
+
       ageFactor.innerText = `Age: ${this.age}`;
-      
+
       this.age++;
 
       if (this.age === 1) {
-        console.log('testing');
+        console.log("testing");
         this.ageUpOne();
       } else if (this.age === 2) {
         this.ageUptwo();
         clearInterval(this.ageid);
       }
     }, 4500);
-
-
   }
-  
- boredomIncrease() {
- 
- this.boredid = setInterval(() => {
-  const boredFactor = document.getElementById('bored');
-  boredFactor.innerText = `Bored: ${this.bored}`;
-  this.bored++;
-  
-  if (this.bored > 10) {
-      clearInterval(this.boredid);
-      this.killedTamagotchi();
-      
+
+  boredomIncrease() {
+    this.boredid = setInterval(() => {
+      const boredFactor = document.getElementById("bored");
+      boredFactor.innerText = `Bored: ${this.bored}`;
+      this.bored++;
+
+      if (this.bored > 10) {
+        clearInterval(this.boredid);
+        this.killedTamagotchi();
       }
-      
     }, 1500);
-    
- }
- ageUpOne() {
-  document.getElementById('pug').setAttribute('src', 'images/pug.png');
-  
-}
-ageUpTwo () {
-  document.getElementById('pug').setAttribute('src', 'images/pug.png');
- 
-}
-
-  
-
-killedTamagotchi = function(){
-  document.getElementById('pug').setAttribute('src', 'images/pug.png');
-  clearInterval(this.ageid);
-  clearInterval(this.sleepid);
-  clearInterval(this.hungerid);
-  clearInterval(this.boredid);
-}
-
-
-
-
-nightTime() {
-  let night = document.createElementById('img');
-  night.setAttribute('src', 'images/Moon.png');
-  night.setAttribute('id', 'moon');
-  document.body.appendChild(night);
-  this.lightsOn = false; // 
-
-}
-dayTime() {
-  this.lightsOn = true;
-  document.getElementById('moon').remove();
-}
-changeLights() {
- this.sleep --
- if (this.lightsOn === true) {
-    this.nightTime();
- } else {
-  this.dayTime();
-  
   }
-}
-feed() {
-this.hunger --
-}
+  ageUpOne() {
+    document.getElementById("pug").setAttribute("src", "images/pug.png");
+  }
+  ageUpTwo() {
+    document.getElementById("pug").setAttribute("src", "images/pug.png");
+  }
 
-play() {
-this.boredid --
-}
-beginGame() {
-this.hungerIncrease()
-this.ageIncrease()
-this.boredomIncrease()
-this.sleepIncrease();
+  killedTamagotchi = function () {
+    document.getElementById("pug").setAttribute("src", "images/pug.png");
+    clearInterval(this.ageid);
+    clearInterval(this.sleepid);
+    clearInterval(this.hungerid);
+    clearInterval(this.boredid);
+  };
+
+  nightTime() {
+    let night = document.createElementById("img");
+    night.setAttribute("src", "images/Moon.png");
+    night.setAttribute("id", "moon");
+    document.body.appendChild(night);
+    this.lightsOn = false; //
+  }
+  dayTime() {
+    this.lightsOn = true;
+    document.getElementById("moon").remove();
+  }
+  changeLights() {
+    this.sleep--;
+    if (this.lightsOn === true) {
+      this.nightTime();
+    } else {
+      this.dayTime();
     }
-    
   }
-  
-  const firstTamagotchi = new Tamagotchi("");
-  console.log(firstTamagotchi);
-  
-  const submitBtn = document.querySelector('button')
-  submitBtn.addEventListener('click', () => {
-    firstTamagotchi.beginGame();
-  
-  })
-  
-document.getElementById('feed').addEventListener('click', () => {
-  firstTamagotchi.feed();
-})
-document.getElementById('lightsOff').addEventListener('click', () => {
-  firstTamagotchi.changeLights();
-})
-document.getElementById('play').addEventListener('click', () => {
-  firstTamagotchi.play();
-})
+  feed() {
+    this.hunger--;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  play() {
+    this.boredid--;
+  }
+  beginGame() {
+    this.hungerIncrease();
+    this.ageIncrease();
+    this.boredomIncrease();
+    this.sleepIncrease();
+  }
 }
+
+const firstTamagotchi = new Tamagotchi("");
+console.log(firstTamagotchi);
+
+const submitBtn = document.querySelector("button");
+submitBtn.addEventListener("click", () => {
+  firstTamagotchi.beginGame();
+});
+
+document.getElementById("feed").addEventListener("click", () => {
+  firstTamagotchi.feed();
+});
+document.getElementById("lightsOff").addEventListener("click", () => {
+  firstTamagotchi.changeLights();
+});
+document.getElementById("play").addEventListener("click", () => {
+  firstTamagotchi.play();
+});
